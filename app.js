55bf37a1b85d15dc7c29ed47445be9d0e1aac507
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-22 20:37:03 -0700

    add inquirer package and create name prompt

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
-// allow access to fs module
-const fs = require('fs');
-// includes the generatePage function from another js file
-const generatePage = require('./src/page-template.js');
+// allow access to inquirer package
+const inquirer = require('inquirer');
 
-// skips over the file pathway info
-const profileDataArgs = process.argv.slice(2);
-// declares the first two array values as name and github variables
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+// // allow access to fs module
+// const fs = require('fs');
+// // includes the generatePage function from another js file
+// const generatePage = require('./src/page-template.js');
 
-// writes the file using the fs module
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// // set function to a variable
+// const pageHTML = generatePage(name, github);
 
-    console.log("Porfolio complete! Check out index.html to see the output!");
-});
+// // writes the file using the fs module
+// fs.writeFile('index.html', pageHTML, err => {
+//     if (err) throw err;
+
+//     console.log("Porfolio complete! Check out index.html to see the output!");
+// });
