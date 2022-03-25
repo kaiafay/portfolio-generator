ba8d7066336e68f82af0b3710f178ad4263db490
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-25 16:32:54 -0700

    create copy file function

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -20,3 +20,20 @@ const writeFile = fileContent => {
         });
     });
 };
+
+// function that copies a file
+const copyFile = () => {
+    return new Promise((resolve, reject) => {
+        fs.copyFile('./src/style.css', './dist/style.css', err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File copied!'
+            });
+        });
+    });
+};
